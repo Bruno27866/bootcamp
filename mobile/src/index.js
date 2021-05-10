c1f94a05d41d227962d9def650402cff2a56ec08
bruno27866 <b.alencar_amaral@yahoo.com>
2021-05-10 14:34:22 -0300

    add new hello

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -6,7 +6,7 @@ export default function App() {
     <>
     <StatusBar barStyle = "light-content" backgroundColor = "#7159c1"/>
     <View style = {styles.container}>
-        <Text style = {styles.title}>Hello GoStack</Text>
+        <Text style = {styles.title}>Hello</Text>
     </View>
     </>
     );
