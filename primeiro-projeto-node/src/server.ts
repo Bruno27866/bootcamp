f861adabe240e9fd70f65e7122fedc0ac898db06
bruno27866 <b.alencar_amaral@yahoo.com>
2021-05-16 16:57:08 -0300

    commit 2

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -3,7 +3,7 @@ import express, { request, response } from "express";
 const app = express ();
 
 app.get('/', (request, response) => {
-    return response.json ({message: 'Helo Beagle'});
+    return response.json ({message: 'Helo Beagle Pankeka'});
 });
 
 app.listen (3333, () => {
