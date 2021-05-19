85e77d33b000034fc5c8300d619b77ed6f4cc285
bruno27866 <b.alencar_amaral@yahoo.com>
2021-05-18 22:21:10 -0300

    tentativa de acerto routes

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,11 +1,11 @@
 import express, { request, response } from "express";
-import appointmentsRouter from "./routes/appointments.routes";
 
 const app = express ();
 
 app.get('/', (request, response) => {
   return response.json ({message: 'Hello Beagles Bela & Pankeka'});
 });
+
 
 app.listen (3333, () => {
     console.log('server started on port 3333');
