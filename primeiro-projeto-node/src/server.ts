da40671fce2fb3892a6dca2294021e6c14eb9d59
bruno27866 <b.alencar_amaral@yahoo.com>
2021-05-18 22:30:41 -0300

    nova alteração

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,11 +1,15 @@
 import express, { request, response } from "express";
+import routes from "./routes";
 
 const app = express ();
 
-app.get('/', (request, response) => {
+app.get('/appointment', (request, response) => {
   return response.json ({message: 'Hello Beagles Bela & Pankeka'});
 });
 
+app.post('/appointment', (request, response) => {
+  return response.json(routes);
+});
 
 app.listen (3333, () => {
     console.log('server started on port 3333');
