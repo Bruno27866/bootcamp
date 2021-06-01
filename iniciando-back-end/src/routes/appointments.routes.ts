9eab71f7665fc6bdb42c592340925941bf99cfc1
bruno27866 <b.alencar_amaral@yahoo.com>
2021-06-01 19:56:26 -0300

    Criando repositorio

--- iniciando-back-end/src/routes/appointments.routes.ts
+++ iniciando-back-end/src/routes/appointments.routes.ts
@@ -1,10 +1,9 @@
 import { request, response, Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointment';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
-
-const appointments: Appointment[] = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.post('/', (request, response) => {
 
@@ -12,17 +11,13 @@ appointmentsRouter.post('/', (request, response) => {
 
   const parsedDate = startOfHour(parseISO(date));
 
-  const findAppointmentInSameDate = appointments.find(appointment =>
-    isEqual(parsedDate, appointment.date),
-    );
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
 
     if(findAppointmentInSameDate) {
       return response.status(400).json({message: "Horário indisponível"});
     }
 
-  const appointment = new Appointment(provider, parsedDate);
-
-  appointments.push(appointment);
+  const appointment = appointmentsRepository.create(provider, parsedDate);
 
   return response.json(appointment);
 });
