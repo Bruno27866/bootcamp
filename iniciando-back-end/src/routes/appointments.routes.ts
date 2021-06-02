df363d6564e8fb64e12edb918099ba9e01840439
bruno27866 <b.alencar_amaral@yahoo.com>
2021-06-01 22:06:06 -0300

    Services & SOLID

--- iniciando-back-end/src/routes/appointments.routes.ts
+++ iniciando-back-end/src/routes/appointments.routes.ts
@@ -1,6 +1,7 @@
 import { request, response, Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import {  parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
@@ -11,23 +12,19 @@ appointmentsRouter.get('/', (request, response) =>{
 });
 
 appointmentsRouter.post('/', (request, response) => {
+  try {
+    const {provider, date} = request.body;
 
-  const {provider, date} = request.body;
+  const parsedDate = parseISO(date);
 
-  const parsedDate = startOfHour(parseISO(date));
+  const createAppointment = new CreateAppointmentService(appointmentsRepository);
 
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
-
-    if(findAppointmentInSameDate) {
-      return response.status(400).json({message: "Horário indisponível"});
-    }
-
-  const appointment = appointmentsRepository.create({
-    provider,
-    date: parsedDate,
-  });
+  const appointment = createAppointment.execute({date: parsedDate, provider});
 
   return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({error: err.message});
+  }
 });
 
 export default appointmentsRouter;
